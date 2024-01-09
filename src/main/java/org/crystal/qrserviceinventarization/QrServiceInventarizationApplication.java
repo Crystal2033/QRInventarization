b35b0b7176bec36dd8fcf7b7a789a8475f73806f
Kulikov Pavel <kulikov-pavlik@bk.ru>
2024-01-09 13:10:44 +0300

    Init commit

---
+++ src/main/java/org/crystal/qrserviceinventarization/QrServiceInventarizationApplication.java
@@ -0,0 +1,13 @@
+package org.crystal.qrserviceinventarization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrServiceInventarizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrServiceInventarizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/crystal/qrserviceinventarization/QrServiceInventarizationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.crystal.qrserviceinventarization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrServiceInventarizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
