dbddf6a71485f3243b61214bc03347c0b688da40
Kulikov Pavel <kulikov-pavlik@bk.ru>
2024-02-06 20:38:08 +0300

    Added some changes for DatabaseStarter

--- src/main/java/org/crystal/qrserviceinventarization/config/DatabaseStarter.java
+++ src/main/java/org/crystal/qrserviceinventarization/config/DatabaseStarter.java
@@ -5,11 +5,17 @@
 
 package org.crystal.qrserviceinventarization.config;
 
-import org.crystal.qrserviceinventarization.database.misc.Converter;
 import org.crystal.qrserviceinventarization.database.model.*;
 import org.crystal.qrserviceinventarization.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 
 @Configuration
 public class DatabaseStarter implements CommandLineRunner {
@@ -21,6 +27,8 @@ public class DatabaseStarter implements CommandLineRunner {
 
     private final ChairRepository chairRepository;
 
+    private final MonitorRepository monitorRepository;
+
     private final CityRepository cityRepository;
 
     private final UserRepository userRepository;
@@ -29,14 +37,37 @@ public class DatabaseStarter implements CommandLineRunner {
                            BranchRepository branchRepository,
                            BuildingRepository buildingRepository,
                            CabinetRepository cabinetRepository,
-                           ChairRepository chairRepository, CityRepository cityRepository, UserRepository userRepository) {
+                           ChairRepository chairRepository, MonitorRepository monitorRepository, CityRepository cityRepository, UserRepository userRepository) {
         this.organizationRepository = organizationRepository;
         this.branchRepository = branchRepository;
         this.buildingRepository = buildingRepository;
         this.cabinetRepository = cabinetRepository;
         this.chairRepository = chairRepository;
+        this.monitorRepository = monitorRepository;
         this.cityRepository = cityRepository;
         this.userRepository = userRepository;
+    }
+
+    private BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
+        Image resultingImage = originalImage.getScaledInstance(targetWidth, targetHeight, Image.SCALE_DEFAULT);
+        BufferedImage outputImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+        outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+        return outputImage;
+    }
+
+    public byte[] toByteArray(BufferedImage bi, String format)
+            throws IOException {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(bi, format, baos);
+        return baos.toByteArray();
+    }
+
+    private byte[] getResizedImageInBytes(String path, int targetWidth, int targetHeight) throws IOException {
+        String[] splittedPathByDot = path.split("\\.");
+        BufferedImage buffImage = ImageIO.read(new File(path));
+        buffImage = resizeImage(buffImage, targetWidth, targetHeight);
+        return toByteArray(buffImage, splittedPathByDot[splittedPathByDot.length - 1]);
     }
 
     @Override
@@ -68,24 +99,44 @@ public class DatabaseStarter implements CommandLineRunner {
                 .build();
         building = buildingRepository.save(building);
 
-        Cabinet cabinet = Cabinet.builder()
+        Cabinet cabinet1 = Cabinet.builder()
                 .id(0L)
                 .name("IT-17")
                 .building(building)
                 .build();
-        cabinet = cabinetRepository.save(cabinet);
 
+        cabinet1 = cabinetRepository.save(cabinet1);
 
-        byte[] image = Converter.fromImageToBytes("C:\\Users\\kulik\\OneDrive\\Изображения\\Saved Pictures\\magicForest.jpg");
-        Chair chair = Chair.builder()
+        Cabinet cabinet2 = Cabinet.builder()
                 .id(0L)
-                .name("Blue chair")
-                .inventoryNumber("ABCD1234")
-                .image(image)
-                .cabinet(cabinet)
+                .name("IT-15")
+                .building(building)
                 .build();
+        cabinet2 = cabinetRepository.save(cabinet2);
 
-        chair = chairRepository.save(chair);
+        byte[] image1 = getResizedImageInBytes("C:\\Users\\kulik\\OneDrive\\Изображения\\Saved Pictures\\cat1.jpg", 200, 200);
+
+//        Chair chair = Chair.builder()
+//                .id(0L)
+//                .name("Blue chair")
+//                .inventoryNumber("ABCD1234")
+//                .image(image1)
+//                .cabinet(cabinet1)
+//                .build();
+//
+//        chair = chairRepository.save(chair);
+//
+//        byte[] image2 = getResizedImageInBytes("C:\\Users\\kulik\\OneDrive\\Изображения\\Saved Pictures\\cat2.jpg", 200, 200);
+//
+//        Monitor monitor = Monitor.builder()
+//                .id(0L)
+//                .name("Black monitor")
+//                .inventoryNumber("5490SDKW")
+//                .image(image2)
+//                .cabinet(cabinet1)
+//                .build();
+//
+//        monitor = monitorRepository.save(monitor);
 
         User user = User.builder()
                 .login("1")
@@ -97,8 +148,6 @@ public class DatabaseStarter implements CommandLineRunner {
                 .build();
 
         userRepository.save(user);
-
-
 
 
     }
